733d0c2d0d08bc9b6167c21615b61132ce645407
austin tabulog <austin.tabulog@gmail.com>
2023-09-25 00:51:35 -0500

    WIP. Added more unit testing and discovered a hidden issue where execute is not being called appropriately after a state change command is issued. Also successfully added accessors for sleep duration data

--- tabby-tools/common/test/ThreadManagerBase_ut.cpp
+++ tabby-tools/common/test/ThreadManagerBase_ut.cpp
@@ -1,4 +1,7 @@
+#include <chrono>
 #include <memory>
+#include <thread>
+#include <iostream>
 #include <catch2/catch_test_macros.hpp>
 #include "ThreadManagerBase.hpp"
 
@@ -20,7 +23,13 @@ public:
     void execute()
     {
         i++;
-        sleep();
+        this->sleep();
+    }
+
+    int get_count()
+    {
+        std::cout << "yeet" << std::endl;
+        return i;
     }
 
 private:
@@ -32,7 +41,7 @@ class ManagerTestFixture
 public:
     ManagerTestFixture()
     {
-        duration_ = std::chrono::microseconds(10000);
+        duration_ = std::chrono::microseconds(50);
         uut_ = MockManager{(int) duration_.count()};
     }
 
@@ -48,26 +57,61 @@ TEST_CASE_METHOD(ManagerTestFixture, "verify start method", "[ThreadManagerBase]
     REQUIRE(uut_.state() == common::ManagedState::Uninitialized);
     uut_.start();
     REQUIRE(uut_.state() == common::ManagedState::Running);
+    
+    std::this_thread::sleep_for (std::chrono::microseconds(1000));
+    REQUIRE(uut_.get_count() > 0);
+    
 }
 
 TEST_CASE_METHOD(ManagerTestFixture, "verify stop method", "[ThreadManagerBase]")
 {
     uut_.start();
     uut_.stop();
+    auto res = uut_.get_count();
     REQUIRE(uut_.state() == common::ManagedState::Terminated);
+
+    std::this_thread::sleep_for (std::chrono::microseconds(1000));
+    REQUIRE(res == uut_.get_count());
 }
 
 TEST_CASE_METHOD(ManagerTestFixture, "verify pause method", "[ThreadManagerBase]")
 {
     uut_.start();
     uut_.pause();
+    auto res = uut_.get_count();
     REQUIRE(uut_.state() == common::ManagedState::Suspended);
+
+    std::this_thread::sleep_for (std::chrono::microseconds(1000));
+    REQUIRE(res == uut_.get_count());
+
 }
 
 TEST_CASE_METHOD(ManagerTestFixture, "verify resume method", "[ThreadManagerBase]")
 {
     uut_.start();
+    std::this_thread::sleep_for (std::chrono::microseconds(1000));
+    
     uut_.pause();
+    auto res = uut_.get_count();
     uut_.resume();
     REQUIRE(uut_.state() == common::ManagedState::Running);
+
+    std::this_thread::sleep_for (std::chrono::microseconds(1000));
+    REQUIRE(res < uut_.get_count());
+}
+
+TEST_CASE_METHOD(ManagerTestFixture, "verify sleep duration accessors", "[ThreadManagerBase]")
+{
+    REQUIRE(uut_.sleep_duration() == std::chrono::microseconds(50));
+
+    //set to new microsecond value
+    auto set_micro = std::chrono::microseconds(500);
+    uut_.sleep_duration(set_micro);
+    REQUIRE(uut_.sleep_duration() == set_micro);
+    
+    //set to new int value
+    int set_int = 5000;
+    uut_.sleep_duration(set_int);
+    REQUIRE(uut_.sleep_duration() == std::chrono::microseconds(set_int));
+
 }
